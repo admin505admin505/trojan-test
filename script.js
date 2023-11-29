b6ede099933a49dd817eacaa4e7a96f6ebd45e19
admin505 <100965441+admin505admin505@users.noreply.github.com>
2023-11-29 10:28:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    const publicIP = data.ip;
-    document.getElementById('public-ip').innerText = publicIP;
-  });
-
-fetch('https://ipapi.co/json/')
-  .then(response => response.json())
-  .then(data => {
-    const postalCode = data.postal;
-    document.getElementById('postal-code').innerText = postalCode;
-  });
+function spamErrorBox(message, numTimes) {
+  for (let i = 0; i < numTimes; i++) {
+    alert(message);
+  }
+}
