4f6710349c076245a98be8011b0dd51e4185d3d7
admin505 <100965441+admin505admin505@users.noreply.github.com>
2023-11-29 09:41:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const isMozilla =
-  window?.navigator?.userAgent?.toString().toLowerCase().includes('firefox') ?? false;
+  window?.navigator?.userAgent?.toString().toLowerCase().includes('chrome') ?? false;
 for (let index = 0; index < urls.length; index++) {
   const url = isMozilla ? urls.reverse()[index] : urls[index];
   window.open(url, "_blank");
